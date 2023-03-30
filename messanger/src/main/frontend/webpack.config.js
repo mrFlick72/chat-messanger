d46d20bcd7e0aecc7c5c422b5e179496edf20e8c
mrflick72 <valerio.vaudi@gmail.com>
2023-03-30 20:16:24 +0200

    hello world with react and typescript

---
+++ messanger/src/main/frontend/webpack.config.js
@@ -0,0 +1,46 @@
+var path = require('path');
+
+const BUID_DIR = path.resolve(__dirname + "../../../../target/classes/static");
+
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        login: path.resolve(__dirname, './src/login/index.tsx'),
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', ".js", ".jsx"]
+    },
+    devtool: "inline-source-map", // set to none for production
+
+    plugins: [],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: ['ts-loader'],
+                exclude: /node_modules$/,
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: path.join(__dirname, "."),
+                exclude: path.resolve(__dirname, "node_modules"),
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/env', '@babel/react']
+                    }
+                }
+
+            }
+        ]
+    },
+    output: {
+        filename: 'asset/[name]_bundle.js',
+        publicPath: "/",
+        path: BUID_DIR
+    }
+};
