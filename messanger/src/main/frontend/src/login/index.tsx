827d1a045907054d53186cf8baf077d55c02b99d
mrflick72 <valerio.vaudi@gmail.com>
2023-03-30 20:16:05 +0200

    hello world with react and typescript

---
+++ messanger/src/main/frontend/src/login/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const Login = () => {
+    return <h1>It Works</h1>
+}
+
+ReactDOM.render(<Login / >, document.getElementById('app'));
