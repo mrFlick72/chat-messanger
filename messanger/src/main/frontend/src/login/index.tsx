8e3d4ac870fb4e93b4c6343740d5938dd9940041
mrflick72 <valerio.vaudi@gmail.com>
2023-04-01 18:09:14 +0200

    login page

--- messanger/src/main/frontend/src/login/index.tsx
+++ messanger/src/main/frontend/src/login/index.tsx
@@ -1,29 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Box, Button, Divider, TextField} from "@mui/material";
+import {Box, Button, Container, Divider, Paper, TextField, Typography} from "@mui/material";
 
 const Login = () => {
-    return <Box>
-        <form action="/login" method="post">
-            <Box>
-                <TextField
-                    name="username"
-                    id="username"
-                    label="Username"
-                    type="text"/>
+    return <Container>
+        <Paper elevation={5}>
+            <Typography variant="h1">Login</Typography>
+            <form action="/login" method="post">
+                <Box>
+                    <div>
+                        <TextField
+                            name="username"
+                            id="username"
+                            label="Username"
+                            type="text"/>
 
-                <TextField
-                    name="password"
-                    id="password"
-                    label="password"
-                    type="password"/>
+                    </div>
+                    <div>
+                        <TextField
+                            name="password"
+                            id="password"
+                            label="password"
+                            type="password"/>
 
-                <Divider/>
+                    </div>
 
-                <Button type="submit"> Login </Button>
-            </Box>
-        </form>
-    </Box>
+                    <Divider/>
+
+                    <Button variant="contained" type="submit"> Login </Button>
+                </Box>
+            </form>
+        </Paper>
+    </Container>
 }
 
 ReactDOM.render(<Login/>, document.getElementById('app'));
