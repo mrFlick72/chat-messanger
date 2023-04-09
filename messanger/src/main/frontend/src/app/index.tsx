851d14029f1820084bdd49344e4f1c9a23b75342
mrflick72 <valerio.vaudi@gmail.com>
2023-04-09 13:57:57 +0200

    create new endpoint scaffold

--- messanger/src/main/frontend/src/app/index.tsx
+++ messanger/src/main/frontend/src/app/index.tsx
@@ -4,17 +4,19 @@ import {Button, Grid, Paper, TextField, Typography} from "@mui/material";
 
 const Application = () => {
     return <Paper elevation={3}>
-        <Grid container spacing={2}>
-            <Grid item xs={12}>
-                <Typography variant={"h3"}>Create a new Room with</Typography>
+        <form action="create-new-room">
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <Typography variant={"h3"}>Create a new Room with</Typography>
+                </Grid>
+                <Grid item xs={4}>
+                    <TextField label="user to invite" fullWidth/>
+                </Grid>
+                <Grid item xs={8}>
+                    <Button type="submit" variant="outlined">Invite</Button>
+                </Grid>
             </Grid>
-            <Grid item xs={4}>
-                <TextField label="user to invite" fullWidth/>
-            </Grid>
-            <Grid item xs={8}>
-                <Button type="button">Invite</Button>
-            </Grid>
-        </Grid>
+        </form>
     </Paper>
 
 }
--- messanger/src/main/kotlin/it/valeriovaudi/messanger/WebComponents.kt
+++ messanger/src/main/kotlin/it/valeriovaudi/messanger/WebComponents.kt
@@ -1,10 +1,12 @@
 package it.valeriovaudi.messanger
 
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PostMapping
 
 
 @Controller
@@ -15,6 +17,9 @@ class IndexController {
 
     @GetMapping("/index")
     fun index() = "index"
+
+    @PostMapping("/create-new-room")
+    fun createNewRoom() = ResponseEntity.ok().build<Unit>()
 }
 
 @Controller
